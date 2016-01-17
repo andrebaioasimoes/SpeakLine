c7d29c0898f04748b57318518020a6778d2609d9
André Simões <abaioasimoes@apple.com>
2016-01-17 22:02:40 +0100

    Initial Commit

---
+++ SpeakLine/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SpeakLine
+//
+//  Created by André Simões on 17/01/16.
+//  Copyright © 2016 Andre Simoes. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
