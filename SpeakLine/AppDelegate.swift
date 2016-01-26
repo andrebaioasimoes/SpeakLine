e1679d9cca2c8a48fdf246aa746c9bd46de5a546
André Simões <abaioasimoes@apple.com>
2016-01-26 20:04:01 +0000

    first commit

--- SpeakLine/AppDelegate.swift
+++ SpeakLine/AppDelegate.swift
@@ -11,17 +11,18 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    @IBOutlet weak var window: NSWindow!
-
-
-    func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+    var mainWindowController: MainWindowController?
+    
+    func applicationDidFinishLaunching(notification: NSNotification) {
+        
+        let mainWindowController = MainWindowController()
+        
+        mainWindowController.showWindow(self)
+        
+        self.mainWindowController = mainWindowController
+        
+        
     }
-
-    func applicationWillTerminate(aNotification: NSNotification) {
-        // Insert code here to tear down your application
-    }
-
 
 }
 
---
+++ SpeakLine/MainWindowController.swift
@@ -0,0 +1,19 @@
+//
+//  MainWindowController.swift
+//  SpeakLine
+//
+//  Created by André Simões on 17/01/16.
+//  Copyright © 2016 Andre Simoes. All rights reserved.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+}
